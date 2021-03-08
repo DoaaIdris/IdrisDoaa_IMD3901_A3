9f157ab24524d634799d1a9b23f4d46d8ab42be5
DoaaIdris <61590380+DoaaIdris@users.noreply.github.com>
2021-03-07 19:24:32 -0500

    updates

--- app.js
+++ app.js
@@ -33,15 +33,48 @@ io.on('connection', (socket) => {
         console.log( socket.id + " disconnected" );
     });
 
-    socket.on("red", (data) => {
-        console.log( "red event received" );
-        io.sockets.emit("color_change", {r:255, g:0, b:0});
+    socket.on("purpleBox1", event => {
+        console.log( "change to purple" );
+        io.sockets.emit("purple1", {r:255, g:0, b:255});
     });
 
-    socket.on("blue", (data) => {
-        console.log( "blue event received" );
-        io.sockets.emit("color_change", {r:0, g:0, b:255});
+    socket.on("purpleBox2", event => {
+        console.log( "change to purple" );
+        io.sockets.emit("purple2", {r:255, g:0, b:255});
     });
+
+    socket.on("redBox", (data) => {
+        console.log( "red box added" );
+        io.sockets.emit("redBox_added");
+    });
+
+    socket.on("blueBox", (data) => {
+        console.log( "blue box added" );
+        io.sockets.emit("blueBox_added");
+    });
+
+    socket.on("changeRed", (data) => {
+        console.log( "change to red box" );
+        io.sockets.emit("redBox_changed", {r:255, g:0, b:0});
+    });
+
+    socket.on("changeBlue", (data) => {
+        console.log( "change to blue box" );
+        io.sockets.emit("blueBox_changed", {r:0, g:0, b:255});
+    });
+
+    socket.on("disableRed", (data) => {
+        console.log( "disable red box" );
+        io.sockets.emit("redBox_disabled", {r:255, g:100, b:100});
+    });
+
+    socket.on("disableBlue", (data) => {
+        console.log( "disable blue box" );
+        io.sockets.emit("blueBox_disabled", {r:100, g:100, b:255});
+    });
+
+
+    
 
     //infinite loop with a millisecond delay (but only want one loop running ...)
     //a way to update all clients simulatenously every frame i.e. updating position, rotation ...
